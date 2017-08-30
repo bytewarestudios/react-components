1c7d332d368b9b48e8108e5b47a8c9097eddb372
Larry Lane <larry.lane@vml.com>
2017-08-29 23:18:48 -0500

    testing login redirect

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React Components</h2>
-        </div>
 
-        <h4> Accordion Component Demo </h4>
-        <Accordion tabTitle="{accordion.titles}"></Accordion>
-      </div>
-    );
+  render() {
+    if (firebaseUserAccount.checkSignedIn()) {
+      return (
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h2>Welcome to React Components</h2>
+          </div>
+
+          <h4> Accordion Component Demo </h4>
+          <Accordion tabTitle="{accordion.titles}"></Accordion>
+        </div>
+      );
+    } else {
+      // user is not authenticate redirect to login page
+      window.location.assign('/login');
+    }
   }
 }
 
